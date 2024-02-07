5927a61aaf66edc4de27d718dd2686b766b78851
EU97 <eurr97@gmail.com>
2024-02-07 17:56:40 -0600

    Main code update

--- Force Workbench 1 axis/src/main.cpp
+++ Force Workbench 1 axis/src/main.cpp
@@ -1,26 +1,13 @@
 #include <Arduino.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
+void setup() {
 
-int main() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
-    const uint led_pin = 25;
-
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-
-    // Initialize chosen serial port
-    stdio_init_all();
-
-    // Loop forever
-    while (true) {
-
-        // Blink LED
-        printf("Blinking!\r\n");
-        gpio_put(led_pin, true);
-        sleep_ms(1000);
-        gpio_put(led_pin, false);
-        sleep_ms(1000);
-    }
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000);                     
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);                      
 }
